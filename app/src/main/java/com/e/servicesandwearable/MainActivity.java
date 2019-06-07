c93090a968426cd1246be4eb1462267ebf406987
Softwarica College Lab <softwarica@softwarica.com>
2019-06-07 07:39:41 +0545

    message send

--- app/src/main/java/com/e/servicesandwearable/MainActivity.java
+++ app/src/main/java/com/e/servicesandwearable/MainActivity.java
@@ -8,6 +8,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+import app.CreateChannel;
+
 public class MainActivity extends AppCompatActivity {
         private Button button1,button2;
         NotificationManagerCompat notificationManagerCompat;
@@ -19,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
         button1 = findViewById(R.id.button);
         button2 = findViewById(R.id.button2);
         notificationManagerCompat = NotificationManagerCompat.from(this);
+        CreateChannel channel = new CreateChannel(this);
+        channel.createChannel();
 
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
     private void DisplayNotification(){
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(MainActivity.this,"Channel1")
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this,CreateChannel.CHANNEL_1)
                 .setSmallIcon(R.drawable.ic_chat_black_24dp)
                 .setContentTitle("First Message")
                 .setContentText("This is my first message")
@@ -47,7 +51,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     private void DisplayNotification2(){
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext(),"Channel2")
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this,CreateChannel.CHANNEL_2)
                 .setSmallIcon(R.drawable.ic_chat_black_24dp)
                 .setContentTitle("Second Message")
                 .setContentText("This is my Second message")
